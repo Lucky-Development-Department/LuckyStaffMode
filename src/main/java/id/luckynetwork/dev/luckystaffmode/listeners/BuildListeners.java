2e10cb60525d5257bc2a56ca1c5963f92fdb89c9
BadAccuracyID <efran.nathanael@gmail.com>
2021-08-25 00:31:38 +0700

    - Improved BuildListeners

--- src/main/java/id/luckynetwork/dev/luckystaffmode/listeners/BuildListeners.java
+++ src/main/java/id/luckynetwork/dev/luckystaffmode/listeners/BuildListeners.java
@@ -2,12 +2,15 @@ package id.luckynetwork.dev.luckystaffmode.listeners;
 
 import id.luckynetwork.dev.luckystaffmode.LuckyStaffMode;
 import id.luckynetwork.dev.luckystaffmode.data.PlayerData;
+import id.luckynetwork.dev.luckystaffmode.utils.CustomItem;
 import lombok.AllArgsConstructor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 
 @AllArgsConstructor
 public class BuildListeners implements Listener {
@@ -18,8 +21,19 @@ public class BuildListeners implements Listener {
     public void onPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
         PlayerData playerData = plugin.getCacheManager().getPlayerData(player);
-        if (playerData.isStaffMode() && !playerData.isCanBuild()) {
-            event.setCancelled(true);
+        if (playerData.isStaffMode()) {
+            ItemStack itemInHand = player.getItemInHand();
+            if (itemInHand != null && itemInHand.getType() != Material.AIR) {
+                CustomItem customItem = plugin.getCacheManager().getCustomItem(itemInHand);
+                if (customItem != null) {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+
+            if (!playerData.isCanBuild()) {
+                event.setCancelled(true);
+            }
         }
     }
 
@@ -27,8 +41,19 @@ public class BuildListeners implements Listener {
     public void onBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
         PlayerData playerData = plugin.getCacheManager().getPlayerData(player);
-        if (playerData.isStaffMode() && !playerData.isCanBuild()) {
-            event.setCancelled(true);
+        if (playerData.isStaffMode()) {
+            ItemStack itemInHand = player.getItemInHand();
+            if (itemInHand != null && itemInHand.getType() != Material.AIR) {
+                CustomItem customItem = plugin.getCacheManager().getCustomItem(itemInHand);
+                if (customItem != null) {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+
+            if (!playerData.isCanBuild()) {
+                event.setCancelled(true);
+            }
         }
     }
 }
